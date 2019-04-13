a0fcc4717e0f04270d05163202bec2c034ac6118
adominguez <dmartin.alberto@gmail.com>
2019-04-13 13:36:34 +0200

    feat(server): add get-spotify-token service

--- server.js
+++ server.js
@@ -1,85 +1,43 @@
 var express = require('express');
-var _ = require('underscore');
-var https = require("https");
-var request = require('request');
+var SpotifyWebApi = require('spotify-web-api-node');
 
 var app = express();
 var PORT = process.env.PORT || 8000;
 
-var client_id = 'afa5db9e6e4e4840ad591f58dc5b6a2d'; // Spotify client id
-var client_secret = '7b231e9b2c524ac08b518761fbfff616'; // Spotify secret
+var clientId = 'afa5db9e6e4e4840ad591f58dc5b6a2d'; // Spotify client id
+var clientSecret = '7b231e9b2c524ac08b518761fbfff616'; // Spotify secret
 
-// Spotify Application requests authorization
-var authOptions = {
-  url: 'https://accounts.spotify.com/api/token',
-  headers: {
-    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
-  },
-  form: {
-    grant_type: 'client_credentials'
-  },
-  json: true
-};
+// Create the api object with the credentials
+var spotifyApi = new SpotifyWebApi({
+  clientId: clientId,
+  clientSecret: clientSecret
+});
 
 
 app.get('/', function (req, res) {
-  res.send('SPOTIFY API');
-})
-
-// GET /printers
-app.get('/printers', function (req, res) {
   res.header('Access-Control-Allow-Origin', "*"); // TODO - Make this more secure!!
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
   res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept');
-  res.json(printers);
-});
-
+  res.send('SPOTIFY API');
+})
 
 // GET TOKEN
-app.get('/get-token', function (req, res) {
-  request.post(authOptions, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      const token = body.access_token;
-      return token;
-    } else {
-      return {
-        type: error,
-        error
-      }
-    }
-  });
-});
+app.get('/get-spotify-token', function (req, res) {
+  // Retrieve an access token.
 
-// GET /artist/:id
-app.get('/artist/:id', function (req, res) {
-  request.post(authOptions, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      const query = req.params.id
-      const url = `https://api.spotify.com/v1/search?q=${query}&type=artist&limit=15`;
-      const token = body.access_token;
-      console.log(token)
-      var options = {
-        url: url,
-        headers: {
-          'Authorization': 'Bearer ' + token
-        },
-        json: true
-      };
-      return request.get(options, function (error, response, body) {
-        if(!error) {
-          //console.log(response.body["artists"].items)
-          return response.body["artists"].items
-        } else {
-          console.log('hay un error', error)
-          return error;
-        }
-      });
-    }
-  });
+  res.header('Access-Control-Allow-Credentials', true); // TODO - Make this more secure!!
+  res.header('Access-Control-Allow-Origin', "*"); // TODO - Make this more secure!!
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST');
+  res.header('Access-Control-Allow-Headers', 'application/json');
+  //res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept');
+  res.json(spotifyApi.clientCredentialsGrant().then(data => {
+    return data.body;
+  }, error => res.json(error))
+  );
 });
 
 // Server port listening
 app.listen(PORT, function () {
   console.log('Express served in the port ' + PORT)
   console.log('http://localhost:' + PORT)
-})
+});
